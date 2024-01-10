3258ead230f63f19c2c6dd8c8eef9745e4e6fab3
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2024-01-10 16:26:13 +0530

    revising promises

---
+++ Javascript topic/CreateOwnPromise.js
@@ -0,0 +1,30 @@
+const cart = ["shoes", "pants", "tshirt"];
+
+const promises = createOrderId(cart);
+
+promises.then(function (orderID) {
+  return proceedToPayment(orderID);
+});
+
+function createOrderId() {
+  const pr = new Promise(function (resolve, reject) {
+    // logic for createOrder
+
+    // eg :
+    if (!validCart(cart)) {
+      const err = new Error("not validated");
+      reject(err);
+    }
+
+    // logic for create order
+
+    const orderID = "123455";
+    if (orderID) {
+      resolve();
+    }
+  });
+
+  return pr;
+}
+
+// Resolve and reject arguments given by javscript by design of promise API.
--- topic/sortAnArray.js
+++ topic/sortAnArray.js
@@ -29,36 +29,36 @@
 
 // console.log(sortAna(nums));
 
-// const nums = [10, 1, 4, 5, 2, 3, 9];
-// function sortAna(nums) {
-//   for (let i = 0; i < nums.length; i++) {
-//     for (let j = 0; j < nums.length; j++) {
-//       if (nums[i] < nums[j]) [nums[i], nums[j]] = [nums[j], nums[i]];
-//     }
-//   }
-//   return nums;
-// }
-
-// console.log(sortAna(nums));
-
-// Quick sort
 const nums = [10, 1, 4, 5, 2, 3, 9];
-function sortQuick(nums) {
-  if (nums.length <= 1) return nums;
-  
-  let pivot = nums[0];
-  let right = [];
-  let left = [];
-
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] < pivot) {
-      left.push(nums[i]);
-    } else {
-      right.push(nums[i]);
+function sortAna(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] < nums[j]) [nums[i], nums[j]] = [nums[j], nums[i]];
     }
   }
-
-  return [...sortQuick(left), pivot, ...sortQuick(right)];
+  return nums;
 }
 
-console.log(sortQuick(nums));
+console.log(sortAna(nums));
+
+// Quick sort
+// const nums = [10, 1, 4, 5, 2, 3, 9];
+// function sortQuick(nums) {
+//   if (nums.length <= 1) return nums;
+  
+//   let pivot = nums[0];
+//   let right = [];
+//   let left = [];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] < pivot) {
+//       left.push(nums[i]);
+//     } else {
+//       right.push(nums[i]);
+//     }
+//   }
+
+//   return [...sortQuick(left), pivot, ...sortQuick(right)];
+// }
+
+// console.log(sortQuick(nums));
