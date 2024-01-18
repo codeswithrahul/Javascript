a924be6a9cff76a3f4b8d4ebb715d1fcec936aa1
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2024-01-18 16:59:32 +0530

    learning AsyncAwait

--- Javascript topic/AsyncAwait.js
+++ Javascript topic/AsyncAwait.js
@@ -1,4 +1,53 @@
-/** What is async ? 
+/** What is async ?
  * Async is a keyword that is used before function to create async function.
  * Async function always return a promise
+ * Async and await used to handle promises
+ * either you return a promise function or if you don't return promise then it will wrap inside the promise then it will return it.
  **/
+
+// eg:1 handle promise using then method
+
+const p = new Promise((resolve, reject) => {
+  resolve("promise has been resolved");
+});
+
+// Above code is returning a promise
+
+/**
+ * Returns a promise that resolves with the result of the asynchronous function.
+ * @param {function} fn - The asynchronous function to execute.
+ * @returns {Promise} A promise that resolves with the result of the asynchronous function.
+ */
+function async(fn) {
+  // ...
+}
+// Above code is also returns a promise so it will not wrap inside promise it will give only one promise.
+getUser().then(function (data) {
+  console.log(data);
+});
+
+//
+
+// eg : 2 handle promise using async await
+const pro = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise has been resolved 2");
+  }, 10000);
+});
+
+// await keyword can only be used inside async function
+
+async function handlePromise() {
+  const getVal = await pro;
+  console.log("namaste js");
+  console.log(getVal);
+}
+
+handlePromise();
+// line no 37 what will be the output?
+// most people think it will quickly print namaste js then it will wait for 
+// 10 second to resolve the promise then it will print promise has been resolve 2
+// but in answer lies here on 36 it will wait for 10second on line not 36 as soon as promise will resolve then it will print
+
+
+
--- Javascript topic/CreateOwnPromise.js
+++ Javascript topic/CreateOwnPromise.js
@@ -2,9 +2,28 @@ const cart = ["shoes", "pants", "tshirt"];
 
 const promises = createOrderId(cart);
 
-promises.then(function (orderID) {
-  return proceedToPayment(orderID);
-});
+promises
+  .then(function (orderID) {
+    console.log(orderID);
+    return orderID;
+  })
+  .then(function (orderID) {
+    return proceedToPayment(orderID);
+  })
+  .then(function (transaction) {
+    console.log(transaction);
+  })
+  .catch(function (err) {
+    // if the promise failed then we have to handle error gracefully. because it gives silently error on browser.
+    console.log(err.message);
+  })
+  .then(function (milGyapayment) {
+    console.log("no matter what it will be called");
+  });
+
+// if we are passing callback function one to another function (chaining). don't forget to write return keyword.
+// any error come up in any promise so that catch will handle automatically.
+// after the catch block , call back will be called no matter what happens in above code or promise.
 
 function createOrderId() {
   const pr = new Promise(function (resolve, reject) {
@@ -28,3 +47,4 @@ function createOrderId() {
 }
 
 // Resolve and reject arguments given by javscript by design of promise API.
+// if we are passing callback function one to another function (chaining). don't forget to write return keyword.
