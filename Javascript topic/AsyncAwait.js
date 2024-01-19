25557ae3a8e756d7acd65d729b1e5cf66d16cda9
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2024-01-19 16:51:04 +0530

    Diving deep in async await

--- Javascript topic/AsyncAwait.js
+++ Javascript topic/AsyncAwait.js
@@ -1,5 +1,5 @@
-/** What is async ?
- * Async is a keyword that is used before function to create async function.
+/** What is async Await?
+ * Async is a keyword that is used before function to create async function and await is a keyword that will used to handle promises.
  * Async function always return a promise
  * Async and await used to handle promises
  * either you return a promise function or if you don't return promise then it will wrap inside the promise then it will return it.
@@ -38,16 +38,94 @@ const pro = new Promise((resolve, reject) => {
 // await keyword can only be used inside async function
 
 async function handlePromise() {
-  const getVal = await pro;
+  console.log("callings");
+  const getVal = await pro; // js engine will waiting for promise to be resolved
   console.log("namaste js");
   console.log(getVal);
 }
 
 handlePromise();
-// line no 37 what will be the output?
-// most people think it will quickly print namaste js then it will wait for 
+// line no 46 what will be the output?
+// most people think it will quickly print namaste js then it will wait for
 // 10 second to resolve the promise then it will print promise has been resolve 2
-// but in answer lies here on 36 it will wait for 10second on line not 36 as soon as promise will resolve then it will print
+// but in answer lies here on 41 it will wait for 10second on line not 41 as soon as promise will resolve then it will print
+// namaste js then promise has been resolved 2
 
+// eg : 3
 
+async function handlePromise() {
+  console.log("callings");
 
+  const getVal = await pro; // js engine will waiting for promise to be resolved
+  console.log("namaste js");
+  console.log(getVal);
+
+  const getVal2 = await pro; // js engine will waiting for promise to be resolved
+  console.log("namaste js2");
+  console.log(getVal2);
+}
+
+handlePromise();
+
+// what will be the output?
+// first it will print callings
+// After 10 seconds both promise will be resolve at once
+// Callings - namaste js - promise has been resolved 2 - namaste js2 - promise has been resolved 2
+
+// eg : 3
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise has been resolved 1");
+  }, 10000);
+});
+
+const p2 = new Promise((res, reject) => {
+  setTimeout(() => {
+    res("promise 2 has been resolved 2");
+  }, 5000);
+});
+
+async function handlePromises() {
+  console.log("inside  async function");
+  const val = await p1;
+  console.log("Namaste Javascript 1");
+  console.log(val);
+
+  const val2 = await p2;
+  console.log("Namaste Javascript 2");
+  console.log(val2);
+}
+handlePromises();
+
+// Line not 92 await p1 this will wait for 10 second to resolve it but another promises should be executed in 5 sec
+// but it will not work like that it will wait for 10 second to resolve the first promise then after it will print 2nd promise
+// Output : inside async function - Namaste 1 - promise has been resolved 1 - Namaste 2 - promise has been resolved 2
+// what if we will change the order of promise first is 5sec and another promise is 10 second?
+//
+// it will behave differently if we change the order so output will be  : -
+// Inside a function
+// after 5 sec it will print Namaste javascript 1
+// promise has been resolved 1
+// after 10 sec it will print Namaste Javascript 2
+// Promise has been resolved 2
+
+// eg : 4
+
+const API_URL = "https://api.github.com/users/rahulchouhan100";
+
+async function handleAPIcall() {
+  try {
+    const data = await fetch(API_URL);
+    const jsonValue = await data.json();
+    console.log(jsonValue);
+  } catch (err) {
+    console.log(err);
+  }
+
+  //fetch () => Response.json() => jsonValue   Note : json returns promise
+  // once the promise is resolved then fetch will give response object has a body which is a readble stream if we want to convert
+  // readle stream to json so we will use .json() and json again return a promise.
+}
+
+handleAPIcall();
--- Javascript topic/PromiseApiInterviewQuestions.js
+++ Javascript topic/PromiseApiInterviewQuestions.js
@@ -1,7 +1,5 @@
-useEffect(() => {
-  
-
-  return () => {
-    second;
-  };
-}, [third]);
+// There are four Promise API which is majorly important
+// 1. Promise.all()
+// 2. Promise.allSettled()
+// 3. Promise.race()
+// 4. Promise.any()
