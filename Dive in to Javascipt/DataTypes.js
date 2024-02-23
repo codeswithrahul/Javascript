06dc4e859472ccb85acaa8588077c21d9bcbbf97
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2024-02-23 13:10:21 +0530

    deep dive in to javascript

---
+++ Dive in to Javascipt/DataTypes.js
@@ -0,0 +1,20 @@
+//  Data types in javascript
+
+// There are two types of data types in javascript
+/* 
+1. Primitive 
+     Number
+     String
+     Boolean
+     undefined
+     null
+     BigIntsymbol
+
+2. Non Primitive
+    
+   
+*/
+
+const data = () => {
+  console.log(testing);
+};
