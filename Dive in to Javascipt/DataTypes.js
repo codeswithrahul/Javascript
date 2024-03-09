6bdc4e8aebe2ef694f051f9951033df5a2450550
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2024-03-09 14:35:13 +0530

    diving in to js : String method

--- Dive in to Javascipt/DataTypes.js
+++ Dive in to Javascipt/DataTypes.js
@@ -1,20 +1,34 @@
 //  Data types in javascript
 
 // There are two types of data types in javascript
-/* 
-1. Primitive 
-     Number
-     String
-     Boolean
-     undefined
-     null
-     BigIntsymbol
 
-2. Non Primitive
+// 1. Primitive 
+//      Number
+//      String
+//      Boolean
+//      undefined
+//      null
+//      BigIntsymbol
+//      Symbol
+// 2. Non Primitive
     
-   
-*/
+// we can check data type using type of
 
-const data = () => {
-  console.log(testing);
-};
+
+let  num = 45;
+console.log(typeof num); // number
+console.log(typeof "hello") // string
+
+// convert number into String 
+num =  45 + ""
+// console.log("Number to String : "+ typeof num) - string
+
+// number to string 
+const numTostr = 745;
+// console.log(typeof String(numTostr)); --- String
+
+// string to number 
+
+const myName = "rahul";
+// console.log(typeof +myName) --- number 
+// console.log(typeof Number(myName))   --- number 
